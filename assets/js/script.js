0e1a9132c8c16672b6345ecc97a0ca84c37d0e50
M Rifki Adi Setiawan <104989419+rifkigg@users.noreply.github.com>
2023-11-29 22:31:39 +0700

    Membuat website menjadi responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // add event tiap multiple element
 
 const addEventOnElement = function (elements, evetType, callback) {
-  for (let i = 0, len = elements .length; i < len; i++) {
+  for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(evetType, callback);
   }
 };
@@ -19,3 +19,25 @@ const toggleNavbar = function () {
 };
 
 addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
+
+// parallax effect
+
+const parallaxElements = document.querySelectorAll("[ data-parallax]");
+
+window.addEventListener("mousemove", (event) => {
+  for (let i = 0, len = parallaxElements.length; i < len; i++) {
+    const movementX =
+      (event.clientX / window.innerWidth) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+    const movementY =
+      (event.clientY / window.innerWidth) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+
+    parallaxElements[i].animate(
+      {
+        transform: `translate(${movementX}px, ${movementY}px)`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  }
+});
